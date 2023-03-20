b7a4ceb7a059b25eecb82bf4fe8a105d1fd12596
Bernardo Amaral <bernardo.amaral85@gmail.com>
2023-03-20 12:54:25 -0300

    Starting the project...

---
+++ ci/clang-lint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Exit immediately if a command exits with a non-zero status.
+set -e
+# Enable the globstar shell option
+shopt -s globstar
+# Make sure we are inside the github workspace
+cd $GITHUB_WORKSPACE
+# Install clang-format
+sudo apt-get -y install clang-format-10
