068d9efe6f0c38f659728531dd9ca6193cd15149
Bernardo Amaral <bernardo.amaral85@gmail.com>
2023-03-21 16:10:03 -0300

    Adjusts on wifi manager code

--- interfone_whatsapp.ino
+++ interfone_whatsapp.ino
@@ -1,8 +1,25 @@
-#include <ESP8266WiFi.h>
+#include <Arduino.h>
 #include <DNSServer.h>
+#include <ESP8266HTTPClient.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiClient.h>
+#include <string.h>
 
 #include "WiFiManager.h"  //https://github.com/tzapu/WiFiManager
+
+ESP8266WiFiMulti WiFiMulti;
+
+const char *WIFI_SSID = "Nome da Sua Rede";
+const char *WIFI_PASSWORD = "Senha da Sua Rede";
+const char *SERVER_URL = "https://api.callmebot.com/whatsapp.php?";
+const char *PHONE = "phone=5511999999999";
+const char *MESSAGE = "&text=Sua+mensagem+de+teste";
+const char *API_KEY = "&apikey=999999999";
+char REQUEST_URL[200];  // URL de requisição
+
+const int VP_PIN = 36;  // Pino VP do ESP32
 
 void configModeCallback(WiFiManager *myWiFiManager) {
     Serial.println("Entered config mode");
@@ -12,6 +29,11 @@ void configModeCallback(WiFiManager *myWiFiManager) {
 
 void setup() {
     Serial.begin(115200);
+
+    strcpy(REQUEST_URL, SERVER_URL);
+    strcat(REQUEST_URL, PHONE);
+    strcat(REQUEST_URL, MESSAGE);
+    strcat(REQUEST_URL, API_KEY);
 
     WiFiManager wifiManager;
     // wifiManager.resetSettings();
@@ -23,6 +45,10 @@ void setup() {
         ESP.reset();
         delay(1000);
     }
+
+    WiFi.mode(WIFI_STA);
+    WiFiMulti.addAP(wifiManager.getWiFiSSID().c_str(),
+                    wifiManager.getWiFiPass().c_str());
 
     Serial.println(F("WIFIManager connected!"));
     Serial.print(F("IP --> "));
@@ -38,5 +64,36 @@ void setup() {
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    HTTPClient http;
+    int voltage = analogRead(VP_PIN);
+
+    float voltageValue = voltage / 4095.0 * 12;
+
+    if ((WiFiMulti.run() == WL_CONNECTED)) {
+        WiFiClient client;
+        HTTPClient http;
+
+        if (voltageValue > 6) {
+            Serial.print("[HTTP] begin...\n");
+            if (http.begin(client, REQUEST_URL)) {
+                Serial.print("[HTTP] GET...\n");
+                int httpCode = http.GET();
+                if (httpCode > 0) {
+                    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+                    if (httpCode == HTTP_CODE_OK ||
+                        httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+                        String payload = http.getString();
+                        Serial.println(payload);
+                    }
+                } else {
+                    Serial.printf("[HTTP] GET... failed, error: %s\n",
+                                  http.errorToString(httpCode).c_str());
+                }
+                http.end();
+            } else {
+                Serial.println("[HTTP] Unable to connect");
+            }
+        }
+    }
+    delay(200);
 }
